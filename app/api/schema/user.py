6a46062bd00978ba93497c30bf0b08a7f4bbe13f
Nkasi-e <nkasijnr@gmail.com>
2023-02-18 17:28:53 +0100

    Added user schema for defining the structure of request and response of data.. added signup and login route for user and JWT authentication for tokenization

---
+++ app/api/schema/user.py
@@ -0,0 +1,51 @@
+import re
+from pydantic import BaseModel, EmailStr, validator
+from fastapi import HTTPException
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    password: str
+
+    @validator("password", pre=True)
+    def check_password(cls, v):
+        if not re.match(
+            r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$", v
+        ):
+            raise HTTPException(
+                status_code=400,
+                detail="Password must have a minimum of 8 characters, 1 Uppercase, 1 lowercase and 1 number",
+            )
+        return v
+
+
+class UserCreateSchema(UserBase):
+    pass
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class LoginUserSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenResponseSchema(BaseModel):
+    email: EmailStr
+    access_token: str
+    token_type: str
+
+    class Config:
+        orm_mode = True
+
+
+class TokenData(BaseModel):
+    id: str
